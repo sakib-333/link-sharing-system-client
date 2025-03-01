9abcdc6f45d653bb9b3f143885ad1120078fb12a
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-03-01 14:35:44 +0600

    Add footer.

--- src/Layouts/HomeLayout.tsx
+++ src/Layouts/HomeLayout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const HomeLayout = () => {
   return (
     <div className="max-w-screen-2xl mx-auto">
       <Navbar />
-      <div>
+      <div className="min-h-screen">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };
