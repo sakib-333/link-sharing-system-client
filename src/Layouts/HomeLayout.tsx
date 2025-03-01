4f4d583660ed9a9d4350c9852a9b33dca1045c49
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-03-01 14:29:11 +0600

    Add navbar.

--- src/Layouts/HomeLayout.tsx
+++ src/Layouts/HomeLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
 
 const HomeLayout = () => {
   return (
     <div>
-      <Outlet />
+      <Navbar />
+      <div>
+        <Outlet />
+      </div>
     </div>
   );
 };
