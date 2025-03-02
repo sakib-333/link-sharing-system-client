6e6a51f38a1b037f9077170277605ce77f2530d4
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-03-02 17:33:04 +0600

    Update footer.

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,26 +1,29 @@
+import { FaFacebookSquare, FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import linkLogo from "/linkLogo.svg";
+import { FaSquareXTwitter } from "react-icons/fa6";
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </nav>
+    <footer className=" bg-neutral text-neutral-content">
+      <div className="footer sm:footer-horizontal  items-center p-4">
+        <aside className="grid-flow-col items-center">
+          <img src={linkLogo} alt="logo" /> <h1>Links</h1>
+        </aside>
+        <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+          <a href="https://github.com/sakib-333">
+            <FaGithubSquare className="text-2xl hover:opacity-60 transition" />
+          </a>
+          <a href="https://github.com/sakib-333">
+            <FaLinkedin className="text-2xl hover:opacity-60 transition" />
+          </a>
+          <a href="https://github.com/sakib-333">
+            <FaFacebookSquare className="text-2xl hover:opacity-60 transition" />
+          </a>
+          <a href="https://github.com/sakib-333">
+            <FaSquareXTwitter className="text-2xl hover:opacity-60 transition" />
+          </a>
+        </nav>
+      </div>
+      <p className="text-xs text-center">Copyright &copy; 2025</p>
     </footer>
   );
 };
